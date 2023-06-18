2ffbd56d05772c2b8b475b37f8af0c2035dedaf3
Thomas_DevArk <github.fggvs@slmail.me>
2023-06-18 16:52:58 +0100

    Redesign

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
-import { Poppins } from "next/font/google";
+import { Archivo } from "next/font/google";
 
-const poppins = Poppins({
+const poppins = Archivo({
     display: "swap",
     weight: ["400", "700"],
     subsets: ["latin"],
@@ -20,7 +20,48 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" className="dark">
-            <body className={poppins.className}>{children}</body>
+            <body className={poppins.className}>
+                <nav className="min-h-12 py-2 gap-2 bg-slate-900 px-4 flex items-center justify-between sm:flex-row flex-col">
+                    <div className="font-bold text-xl text-white grow">
+                        TheGoated.Dev
+                    </div>
+                    <div className="flex items-center justify-center gap-4 grow">
+                        <a
+                            href="https://www.linkedin.com/in/thomas-burridge-devark/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-slate-300 hover:text-white transition-opacity"
+                        >
+                            LinkedIn
+                        </a>
+                        <a
+                            href="https://github.com/TheGoatedDev"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-slate-300 hover:text-white transition-opacity"
+                        >
+                            Github
+                        </a>
+                        <a
+                            href="https://twitter.com/TheGoatedDev"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-slate-300 hover:text-white transition-opacity"
+                        >
+                            Twitter
+                        </a>
+                    </div>
+                    <div className="grow flex justify-end">
+                        <a
+                            href="mailto:me@thegoated.dev"
+                            className="bg-brand-gradient text-white px-4 py-1 rounded-full font-bold hover:opacity-80 transition-opacity"
+                        >
+                            Lets Talk
+                        </a>
+                    </div>
+                </nav>
+                {children}
+            </body>
         </html>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,55 @@
-import { IconBrandLinkedin, IconMail } from "@tabler/icons-react";
+import {
+    IconArrowDown,
+    IconBrandLinkedin,
+    IconMail,
+    IconStar,
+    IconStarFilled,
+} from "@tabler/icons-react";
 import Image from "next/image";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center p-4 md:p-24">
-            <div className="flex flex-col items-center justify-center gap-8 md:flex-row md:text-left text-center">
-                <Image
-                    src="/ProfilePicture.jpeg"
-                    alt="The Goated Dev Profile Picture"
-                    width={200}
-                    height={200}
-                    className="rounded-full w-48 h-48  mb-2"
-                />
-                <div className="flex flex-col gap-1">
-                    <h1 className="text-4xl font-bold">The Goated Dev 🐐</h1>
-                    <p className="text-xl text-gray-400">
-                        Versatile Full-Stack and Mobile App Engineer and IoT
-                        Expert: Transforming Energy Management with Real-time
-                        Solutions.
-                    </p>
-                    <div className="flex gap-4 mt-4 justify-center md:justify-start">
-                        <a
-                            href="https://www.linkedin.com/in/thomas-burridge-software/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 rounded-full transition-colors"
-                        >
-                            <IconBrandLinkedin />
-                        </a>
-                        <a
-                            href="mailto:thomas@devarksolutions.com"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="bg-purple-500 hover:bg-purple-700 text-white font-bold p-2 rounded-full transition-colors"
-                        >
-                            <IconMail />
-                        </a>
-                    </div>
+        <main>
+            <div className="py-2 px-4 h-[calc(100vh-8rem)] sm:h-[calc(100vh-4rem)] flex items-center justify-center sm:justify-evenly flex-col sm:flex-row gap-8 relative">
+                <h1 className="sm:w-1/3 text-center sm:text-left">
+                    Hello, I&apos;m{" "}
+                    <span className="text-brand-gradient">Thomas,</span>
+                    <br />
+                    Software Engineer Contractor based in the UK
+                </h1>
+                <div className="md:w-[400px] w-[250px] aspect-square relative">
+                    <Image
+                        className="rounded-full"
+                        src={"/ProfilePicture.jpeg"}
+                        fill
+                        alt="Profile Picture"
+                    />
                 </div>
+                <IconArrowDown
+                    size={48}
+                    className="absolute left-1/2 bottom-2 -translate-x-1/2"
+                />
+            </div>
+
+            <div className="flex items-center justify-center gap-4 min-h-32 bg-brand-gradient py-4 flex-col my-4">
+                <h2 className="text-white -my-2">I Develop</h2>
+                <div className="flex w-full bg-white min-h-24 -skew-y-1 shadow-xl justify-evenly items-center flex-wrap gap-4 py-2 px-2">
+                    <h2>Web Apps</h2>
+                    <IconStarFilled />
+                    <h2>MVPs</h2>
+                    <IconStarFilled />
+                    <h2>IoT Apps</h2>
+                    <IconStarFilled />
+                    <h2>Backends</h2>
+                    <IconStarFilled />
+                    <h2>Frontends</h2>
+                    <IconStarFilled />
+                    <h2>Dashboards</h2>
+                </div>
+            </div>
+
+            <div className="flex items-center justify-center gap-4 h-32 bg-brand-gradient flex-col">
+                <h2 className="text-white">Still Under-development</h2>
             </div>
         </main>
     );
