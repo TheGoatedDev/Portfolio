195ea1de5449b7f32d6ad97f6b948540fb017238
Thomas Burridge <github@login.thegoated.dev>
2026-05-09 17:22:50 +0100

    Remove impeccable live development artifacts
    - Delete .impeccable-live.json config
    - Remove injected live.js script from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,10 +96,7 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} ${sourceSerif.variable} font-sans`}
 			>
 				{children}
-			{/* impeccable-live-start */}
-<script src="http://localhost:8400/live.js"></script>
-{/* impeccable-live-end */}
-</body>
+			</body>
 		</html>
 	);
 }
